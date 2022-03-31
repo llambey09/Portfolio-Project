df292ecd5e7ca335d1f7c8e1fefdaf6c04e96fd1
Lisani Lambey <lisani@gitzofficesupplies.com>
2022-03-31 13:50:58 -0600

    Sanity and react

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+// import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
 
-import './About.scss'
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
 
-const abouts = [
-  {title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
-  {title: 'Web Design', description: 'I am a good web developer', imgUrl: images.about02},
-  {title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03},
-  {title: 'Web Animation', description: 'I am a good web developer', imgUrl: images.about04}
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
 
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
-
-]
-
-const about = () => {
   return (
     <>
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
@@ -29,14 +29,20 @@ const about = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={(about.imgUrl)} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default about
+export default About
+
+// export default AppWrap(
+//   MotionWrap(About, 'app__about'),
+//   'about',
+//   'app__whitebg',
+// );
